bc8a626d948a31b55858d4f4acdd31ee9a088fc7
pinfort <pinfo.walter@gmail.com>
2017-04-18 01:33:25 +0900

    reloadし忘れ修正

--- SSLcert.sh
+++ SSLcert.sh
@@ -3,5 +3,5 @@
 sudo yum install epel-release
 sudo yum install certbot
 sudo firewall-cmd --add-port=443/tcp --zone=public
-sudo firewall-cmd --add-port=443/tcp --zone=public --permanent
+sudo firewall-cmd --reload
 sudo certbot certonly --standalone
